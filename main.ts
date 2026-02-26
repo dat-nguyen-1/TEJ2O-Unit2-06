e5d3090268a965ad8c41223ce2314c1aa567893d
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-02-26 12:48:45 -0500

    button a and button b controls LED

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Dat Nguyen
+ * Created on: Feb 2026
+ * This program uses an SR latch to control an LED.
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+pins.digitalWritePin(DigitalPin.P16, 0)
+
+input.onButtonPressed(Button.A, function() {
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
